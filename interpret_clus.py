f216cec996a0b6fe7547295de1f672018739d893
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2023-12-11 12:33:34 +0100

    Interpret cluster

---
+++ interpret_clus.py
@@ -0,0 +1,149 @@
+from utilities import *
+parts_to_ignore = ["subdir", "file", "cluster", "size"]
+
+def make_avg(part1, part2, thr, subdirnames = [], filenames = [], subdirnames_skip = [], filenames_skip = []):
+     for subdirname in os.listdir(part1):
+        skipping = False
+        for s in subdirnames:
+            if s not in subdirname:
+                skipping = True
+                break
+        for s in subdirnames_skip:
+            if s in subdirname:
+                skipping = True
+                break
+        if skipping:
+            continue
+        print(part1 + subdirname)
+        for filename in os.listdir(part1 + subdirname + "/filenames"):
+            skipping = False
+            for f in filenames:
+                if f not in filename:
+                    skipping = True
+                    break
+            for f in filenames_skip:
+                if f in filename:
+                    skipping = True
+                    break
+            if skipping:
+                continue
+            print(part1 + subdirname + part2 + filename)
+            file_clus_var = pd.read_csv(part1 + subdirname + part2 + filename + "_var.csv", index_col = False) 
+            file_clus_std = pd.read_csv(part1 + subdirname + part2 + filename + "_std.csv", index_col = False) 
+            file_clus_var_scaled = pd.read_csv(part1 + subdirname + part2 + filename + "_var_scaled.csv", index_col = False) 
+            file_clus_std_scaled = pd.read_csv(part1 + subdirname + part2 + filename + "_std_scaled.csv", index_col = False) 
+            file_clus_avg = pd.read_csv(part1 + subdirname + part2 + filename + "_avg.csv", index_col = False)  
+            file_clus_min = pd.read_csv(part1 + subdirname + part2 + filename + "_min.csv", index_col = False)  
+            file_clus_max = pd.read_csv(part1 + subdirname + part2 + filename + "_max.csv", index_col = False) 
+            file_clus_range = pd.read_csv(part1 + subdirname + part2 + filename + "_range.csv", index_col = False)
+
+            clusters = dict()
+            for i, cluster in enumerate(list(file_clus_var["cluster"])):
+                clusters[cluster] = i
+            cluster_keys = sorted(list(clusters.keys()))
+            features = []
+            for feature in file_clus_var.head():
+                if feature not in parts_to_ignore:
+                    features.append(feature)
+
+            min_min_for_feature = dict()
+            max_max_for_feature = dict()
+            max_range_for_feature = dict()
+            for feature in features:
+                min_min_for_feature[feature] = 1000000
+                for mini in file_clus_min[feature]:
+                    min_min_for_feature[feature] = min(min_min_for_feature[feature], mini)
+                max_max_for_feature[feature] = 0
+                for maxi in file_clus_max[feature]:
+                    max_max_for_feature[feature] = max(max_max_for_feature[feature], maxi)
+                max_range_for_feature[feature] = max_max_for_feature[feature] - min_min_for_feature[feature]
+
+            relative_average_for_cluster_feature = dict()
+            for i, cluster in enumerate(cluster_keys): 
+                relative_average_for_cluster_feature[cluster] = dict()
+
+            for i, cluster in enumerate(cluster_keys): 
+                for j, cluster_second in enumerate(cluster_keys[i + 1:]): 
+                    relative_average_for_cluster_feature[cluster][cluster_second] = dict() 
+                    relative_average_for_cluster_feature[cluster_second][cluster] = dict()
+
+            for i, cluster in enumerate(cluster_keys): 
+                for j, cluster_second in enumerate(cluster_keys[i + 1:]): 
+                    for feature in features: 
+                        if max_range_for_feature[feature] != 0:
+                            value = abs(file_clus_avg[feature][clusters[cluster]] - file_clus_avg[feature][clusters[cluster_second]]) / max_range_for_feature[feature]
+                            relative_average_for_cluster_feature[cluster][cluster_second][feature] = value
+                            relative_average_for_cluster_feature[cluster_second][cluster][feature] = value
+            
+            print(len(clusters))
+            for i, cluster in enumerate(cluster_keys): 
+                for j, cluster_second in enumerate(cluster_keys[i + 1:]): 
+                    for feat in dict(sorted(relative_average_for_cluster_feature[cluster][cluster_second].items(), key = lambda item: item[1], reverse = True)): 
+                        if relative_average_for_cluster_feature[cluster][cluster_second][feat] > thr:
+                            print(cluster, cluster_second, feat, relative_average_for_cluster_feature[cluster][cluster_second][feat])
+                            break
+    
+def make_var(part1, part2, feature_name, subdirnames = [], filenames = [], subdirnames_skip = [], filenames_skip = []):
+    for subdirname in os.listdir(part1):
+        skipping = False
+        for s in subdirnames:
+            if s not in subdirname:
+                skipping = True
+                break
+        for s in subdirnames_skip:
+            if s in subdirname:
+                skipping = True
+                break
+        if skipping:
+            continue
+        print(part1 + subdirname)
+        for filename in os.listdir(part1 + subdirname + "/filenames"):
+            skipping = False
+            for f in filenames:
+                if f not in filename:
+                    skipping = True
+                    break
+            for f in filenames_skip:
+                if f in filename:
+                    skipping = True
+                    break
+            if skipping:
+                continue
+            print(part1 + subdirname + part2 + filename)   
+            file_clus_var_scaled = pd.read_csv(part1 + subdirname + part2 + filename + feature_name + ".csv", index_col = False)  
+
+            clusters = dict()
+            for i, cluster in enumerate(list(file_clus_var_scaled["cluster"])):
+                clusters[cluster] = i
+            cluster_keys = sorted(list(clusters.keys()))
+            features = []
+            for feature in file_clus_var_scaled.head():
+                if feature not in parts_to_ignore:
+                    features.append(feature)
+            stdevs_for_cluster = dict()
+            for cluster in cluster_keys:  
+                stdevs_for_cluster[cluster] = dict()
+                for feature in features: 
+                    if not math.isnan(file_clus_var_scaled[feature][clusters[cluster]]) and not math.isinf(file_clus_var_scaled[feature][clusters[cluster]]):
+                        stdevs_for_cluster[cluster][feature] = file_clus_var_scaled[feature][clusters[cluster]] 
+                        stdevs_for_cluster[cluster][feature] = file_clus_var_scaled[feature][clusters[cluster]]
+                for feat in dict(sorted(stdevs_for_cluster[cluster].items(), key = lambda item: item[1], reverse = True)):
+                    print(cluster, feat, stdevs_for_cluster[cluster][feat])
+                    break 
+
+subdirnames = ["all", "poly", "heading", "acceler", "no_xy"]
+filenames = ["KMeans", "nclus 8"]
+subdirnames_skip = ["flags"]
+filenames_skip = ["_train"]
+feature_name = "_std_scaled"
+thr = 0.5
+
+part1 = "all_clus/"
+part2 = "/output_clus/"
+#make_avg(part1, part2, thr, subdirnames, filenames, subdirnames_skip, filenames_skip)
+#make_var(part1, part2, feature_name, subdirnames, filenames, subdirnames_skip, filenames_skip)
+
+part1 = "all_isomap/"
+part2 = "/output_iso/"  
+#make_avg(part1, part2, thr, subdirnames, filenames, subdirnames_skip, filenames_skip)
+make_var(part1, part2, feature_name, subdirnames, filenames, subdirnames_skip, filenames_skip)
--- test_clus.py
+++ test_clus.py
@@ -9,47 +9,83 @@ entries_all = {"labels_window_size_zero_dict": labels_window_size_zero_dict,
                "labels_window_size_max_dict": labels_window_size_max_dict,
                "percents_window_size_max_dict": percents_window_size_max_dict}
 
-for subdirname in os.listdir("all_clus/"):
-    print("all_clus/" + subdirname)
-    largest_h = (0, "")
-    largest_nmi = (0, "")
-    for filename in os.listdir("all_clus/" + subdirname + "/filenames"): 
-        if not "test nclus " in filename:
+def make_list_labels(part1, subdirnames = [], filenames = [], subdirnames_skip = [], filenames_skip = []):
+    for subdirname in os.listdir(part1):
+        skipping = False
+        for s in subdirnames:
+            if s not in subdirname:
+                skipping = True
+                break
+        for s in subdirnames_skip:
+            if s in subdirname:
+                skipping = True
+                break
+        if skipping:
             continue
-        if not "_test" in filename:
-            continue
-        fname = load_object("all_clus/" + subdirname + "/filenames/" + filename)
-        cluster_test = load_object("all_clus/" + subdirname + "/clus_test/" + filename.replace("filenames_in_cluster_test ", "clus_test_"))
-        labels_true = dict()
-        labels_new = []
-        for cluster in fname:
-            for index in range(len(fname[cluster])):
-                name_file = fname[cluster][index]["short_name"]
-                name_file_long = name_file.replace("/", "/cleaned_csv/")  
-                wnd = fname[cluster][index]["window"] 
-                pos = fname[cluster][index]["start"]
-                labels_new.append(cluster)
-                for entry in entries_all:
-                    if entry not in labels_true:
-                        labels_true[entry] = dict()
-                    for prop in entries_all[entry][wnd][name_file_long.split("/")[0]][name_file_long.split("/")[-1]][pos]:
-                        if prop not in labels_true[entry]:
-                            labels_true[entry][prop] = []
-                        labels_true[entry][prop].append(float(entries_all[entry][wnd][name_file_long.split("/")[0]][name_file_long.split("/")[-1]][pos][prop]))
-        
-        labels_new.append(0)
-        labels_new.append(1)
-        for e in labels_true:
-            for p in labels_true[e]:
-                labels_true[e][p].append(1)
-                labels_true[e][p].append(0)
-        for e in labels_true:
-            for p in labels_true[e]:
-                nmi = normalized_mutual_info_score(labels_true[e][p], labels_new)
-                h = homogeneity_score(labels_true[e][p], labels_new)
-                if h > largest_h[0]:
-                    largest_h = (h, p, filename) 
-                if nmi > largest_nmi[0]:
-                    largest_nmi = (nmi, p, filename)
-    print(largest_nmi, largest_h)
+        print(part1 + subdirname)
+        largest_nmi_all, largest_h_all = (0, ""), (0, "")
+        for filename in os.listdir(part1 + subdirname + "/filenames"):
+            skipping = False
+            for f in filenames:
+                if f not in filename:
+                    skipping = True
+                    break
+            for f in filenames_skip:
+                if f in filename:
+                    skipping = True
+                    break
+            if skipping:
+                continue
+            print(part1 + subdirname + "/filenames/" + filename)
+            fname = load_object(part1 + subdirname + "/filenames/" + filename)
+            largest_nmi, largest_h = (0, ""), (0, "")
+            #cluster_test = load_object(part1 + subdirname + "/clus_test/" + filename.replace("filenames_in_cluster_test ", "clus_test_"))
+            labels_true = dict()
+            labels_new = []
+            for cluster in fname:
+                for index in range(len(fname[cluster])):
+                    name_file = fname[cluster][index]["short_name"]
+                    name_file_long = name_file.replace("/", "/cleaned_csv/")  
+                    wnd = fname[cluster][index]["window"] 
+                    pos = fname[cluster][index]["start"]
+                    labels_new.append(cluster)
+                    for entry in entries_all:
+                        if entry not in labels_true:
+                            labels_true[entry] = dict()
+                        for prop in entries_all[entry][wnd][name_file_long.split("/")[0]][name_file_long.split("/")[-1]][pos]:
+                            if prop not in labels_true[entry]:
+                                labels_true[entry][prop] = []
+                            labels_true[entry][prop].append(float(entries_all[entry][wnd][name_file_long.split("/")[0]][name_file_long.split("/")[-1]][pos][prop]))
+            
+            labels_new.append(0)
+            labels_new.append(1)
+            for e in labels_true:
+                for p in labels_true[e]:
+                    labels_true[e][p].append(1)
+                    labels_true[e][p].append(0)
+            for e in labels_true:
+                for p in labels_true[e]:
+                    nmi = normalized_mutual_info_score(labels_true[e][p], labels_new)
+                    h = homogeneity_score(labels_true[e][p], labels_new)
+                    if h > largest_h[0]:
+                        largest_h = (h, p, filename) 
+                    if nmi > largest_nmi[0]:
+                        largest_nmi = (nmi, p, filename)
+                    if h > largest_h_all[0]:
+                        largest_h_all = (h, p, filename) 
+                    if nmi > largest_nmi_all[0]:
+                        largest_nmi_all = (nmi, p, filename)
+            print(largest_nmi, largest_h)
+        print("TOTAL", largest_nmi_all, largest_h_all)
  
+subdirnames = ["all", "poly", "heading", "acceler", "no_xy"]
+filenames = ["KMeans", "nclus 8"]
+subdirnames_skip = ["flags"]
+filenames_skip = ["_train"] 
+
+part1 = "all_clus/"
+part2 = "/output_clus/"
+make_list_labels(part1, subdirnames, filenames, subdirnames_skip, filenames_skip)
+part1 = "all_isomap/"
+part2 = "/output_iso/"
+make_list_labels(part1, subdirnames, filenames, subdirnames_skip, filenames_skip)
